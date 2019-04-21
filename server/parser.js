c9b3be953868efdd07a9fbcc40fe7f00881e59de
Caleb Babin <object.caleb@gmail.com>
2019-04-21 01:42:20 -0500

    adds basic serverside input validation

--- server/parser.js
+++ server/parser.js
@@ -1,8 +1,10 @@
 module.exports = (socket, message, responseBuffer) => {
-    //add input validation and sanitization here.
-
     try {
         const data = JSON.parse(message);
+        const output = {
+            x: Math.max(0, Math.min(1, Number(data.x))),
+            y: Math.max(0, Math.min(1, Number(data.y))),
+        }
         responseBuffer.addEvent(socket.clientId, data);
     } catch(e) {
         console.error( new Error(e));
